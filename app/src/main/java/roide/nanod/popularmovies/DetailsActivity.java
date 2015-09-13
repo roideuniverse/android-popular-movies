0eb3e380e820401ec50ca47e05c778866a3aa24b
Kaushik Saurabh <kaushik@carrotcreative.com>
2015-09-13 08:25:21 -0400

    add Details blank activity and fragment

---
+++ app/src/main/java/roide/nanod/popularmovies/DetailsActivity.java
@@ -0,0 +1,17 @@
+package roide.nanod.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class DetailsActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
